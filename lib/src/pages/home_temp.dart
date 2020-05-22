7de3d6c9cde09a3a806ee2fd0378e62e3e63390a
Roberto <rmp0046@gmail.com>
2020-05-21 23:28:26 -0400

    Create list with ListView widget

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-
-
 class HomePage extends StatelessWidget {
+  final List<String> options = ["One", "Two", "Three", "Four", "Five"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home Temp"),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Components List"),
+        ),
+        body: ListView(
+          children: _createItems(),
+        ));
+  }
+
+  List<Widget> _createItems() {
+    return options
+        .map((item) => Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text(item),
+                  subtitle: Text("1"),
+                  leading: Icon(Icons.access_alarm),
+                  trailing: Icon(Icons.arrow_right),
+                  onTap: (){},
+                ),
+                Divider()
+              ],
+            ))
+        .toList();
   }
 }
